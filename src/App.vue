417955b8660e1be0e5b00d854abb87058cbe93c7
filippogiovannoni <filippogiovannonii@gmail.com>
2024-01-25 12:25:12 +0100

    Initial commit add Hello World

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello Vue!',
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>
+    {{ message }}
+  </h1>
+</template>
+
+<style></style>
